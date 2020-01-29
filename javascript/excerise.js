f44e653a70a7310ba25282392dc8398c7fb1f3be
Tim <timwjones98@gmail.com>
2020-01-29 14:02:05 -0500

    excersies

---
+++ javascript/excerise.js
@@ -0,0 +1,29 @@
+//class exercises
+let sum = 0;
+for(let x=1; x <11; x++){
+  sum = sum + x*x;
+}
+console.log(sum);
+
+let y = 1;
+let prev = 1;
+while(y < 1000){
+  y === y + prev;
+}
+console.log(y);
+
+for(let x=1; x <101; x++){
+  if(x%3 === 0 && x % 5=== 0){
+    x++;
+  }
+  if(x%3 === 0){
+    cosole.log(x)
+    x++;
+  }
+  if(x%5 === 0){
+    cosole.log(x)
+    x++;
+  }else {
+    x++;
+  }
+}
