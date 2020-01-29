b1e4df34e35b549726b7e8a455b014d294be7454
Tim <timwjones98@gmail.com>
2020-01-29 13:47:41 -0500

    little change

---
+++ javascript/statements.js
@@ -0,0 +1,29 @@
+// satements in Javascript
+
+// Constants
+const pi = 3.14;
+
+//printing
+console.log(pi);
+
+//for loop
+for(let x=0; x <3; x++){
+  console.log(x);
+}
+
+//while loop
+
+let x = 0;
+while(x<3){
+  console.log(x);
+  x+=1;
+}
+
+//conditional
+
+if(x===3){
+  console.log('x is now 3');
+} else {
+  console.log(`x is now ${x}`)
+
+}
