92605724bcd60160a2e22907b8924fd10589a607
Tim <timwjones98@gmail.com>
2020-02-03 13:54:42 -0500

    attempt 5

--- javascript/moreecersises.js
+++ javascript/moreecersises.js
@@ -6,11 +6,12 @@ const aClass = function(dept, number){
 };
 
 const classes = [CS220 = new aClass('CS', 220), CS364 = new aClass('CS', 362), CS332 = new aClass('CS', 332)];
-
+let largestnum = 0;
 for(const Class of classes){
   let largestnum = 0;
   if(Class.number > largestnum){
     largestnum = Class.number;
   }
-	console.log(largestnum);
+
 }
+console.log(largestnum);
