a8f6087e9a40a84253a0a475372fd0e0ef182c38
Tim <timwjones98@gmail.com>
2020-02-03 12:52:48 -0500

    excerxies and objects

--- javascript/excerise.js
+++ javascript/excerise.js
@@ -1,31 +1,40 @@
 //class exercises
+
+//1
 let sum = 0;
 for(let x=1; x <11; x++){
   sum = sum + x*x;
 }
 console.log(sum);
 
-let y = 1;
+//2
+let num = 2;
 let prev = 1;
-while(y < 1000){
-  y === y + prev;
-  prev === y;
+while(num < 1000){
+  temp = num + prev;
+  prev = num;
+  num = temp;
 }
-console.log(y);
+console.log(num);
 
-
+//3
+let count =0;
 for(let x=1; x <101; x++){
   if(x%3 === 0 && x % 5=== 0){
     x++;
   }
   if(x%3 === 0){
-    cosole.log(x)
+    count++;
     x++;
   }
   if(x%5 === 0){
-    cosole.log(x)
+    count++;
     x++;
   }else {
     x++;
   }
 }
+console.log(count);
+
+//4
+console.log(10 * 9 * 8 * 7 * 6);
