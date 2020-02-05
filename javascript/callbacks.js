42994de5711a9ef14bd691140d17a1a25022c77c
Tim <timwjones98@gmail.com>
2020-02-05 13:32:41 -0500

    javascript arrow functions

--- javascript/callbacks.js
+++ javascript/callbacks.js
@@ -20,3 +20,12 @@ const numbers = [1, 2, 3, 4, 5, 6];
 // do the transformation
 console.log(map(numbers, square));
 console.log(numbers)
+
+//defining other transformations with arrow functions
+console.log(map(numbers, x=> x**2));
+console.log(map(numbers, x=> x+2));
+
+
+//using array map method
+console.log(numbers.map(x => x=> x**2));
+console.log(numbers.map(x => x=> x+2));
