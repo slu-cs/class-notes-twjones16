415d44c78f466db0db49ff238f083a81ceb4ed09
Tim <timwjones98@gmail.com>
2020-02-10 13:21:23 -0500

    connect to db

---
+++ mongo/faculty/db.js
@@ -0,0 +1,23 @@
+// connect to a database about faculty
+
+const mongoose = require('mongoose');
+//explore this funciton
+
+module.exports = function(){
+  
+
+// Avoid warnings
+    mongoose.set('useNewUrlParser', true);
+    mongoose.set('useUnifiedTopology', true);
+    mongoose.set('useCreateIndex', true);
+    mongoose.set('useFindAndModify', false);
+
+// Start connecting
+    mongoose.connect('mongodb://localhost/faculty');
+
+// Make sure we see any errors
+    mongoose.connection.on('error', function(error) {
+        console.error(error.stack);
+    });
+
+}
