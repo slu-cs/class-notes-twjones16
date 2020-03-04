88ca1e64df864f3b4beaa5925881341fc544adac
Tim <timwjones98@gmail.com>
2020-03-04 13:35:28 -0500

    these are express routes ;)

--- express/hello/app.js
+++ express/hello/app.js
@@ -5,9 +5,32 @@ const express = require('express');
 const app = express();
 
 // Respond to one request
+// http://cs-linuxlab-35.stlawu.local:3000/
 app.get('/', function(request, response) {
-  response.send('Hello Lisa');
+  response.send('Hello Tim');
 });
+
+//cs-linuxlab-35.stlawu.local:3000/foo
+app.get('/foo', function(request, response) {
+  response.send('Hello Tim you are chill. Also foo');
+});
+
+//cs-linuxlab-35.stlawu.local:3000/foo/bar
+app.get('/foo/bar', function(request, response) {
+  response.send('Hello Tim you are chill. Also bar');
+});
+
+//cs-linuxlab-35.stlawu.local:3000/zap/:z
+app.get('/zap/:z', function(request, response) {
+  response.send(`Hello ${request.params.z}`);
+});
+
+//cs-linuxlab-35.stlawu.local:3000/zap/zap?x=X&y=Y
+app.get('/zap', function(request, response) {
+  response.send(`Hello zap with ${request.params.x} and ${request.query.y}`);
+});
+
+
 
 // Start the server
 app.listen(3000);
